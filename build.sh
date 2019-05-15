9aebd6624f9ea0453035bcff0059eea5001e69bd
Indrek Priks <indrek.priks@sympower.net>
2019-05-15 18:00:38 +0300

    Make CircleCI build with Docker, given a unified build.sh script

--- build.sh
+++ build.sh
@@ -1,12 +1,43 @@
 #!/bin/bash -e
 
-printf "\nBuilding Docker image to compile binaries for Linux ...\n\n"
-build/linux/build-image.sh
+# Check the operating environment to know whether 'winpty' needs to be prepended to some docker commands in some Windows unix-like terminals
+UNAME_OUT="$(uname -s)"
+case "${UNAME_OUT}" in
+    CYGWIN*)    WINPTY=winpty;;
+    MINGW*)     WINPTY=winpty;;
+    *)          WINPTY=""
+esac
+printf "\n>> Setting WINPTY=${WINPTY}\n\n"
 
-printf "\nRunning Docker image to compile binaries for Linux ...\n\n"
-build/linux/run-image.sh
+printf "\n>> Building Docker image to compile binaries for Linux ...\n\n"
+DOCKER_BUILDKIT=1 $WINPTY docker build --tag=phoneme-preverifier-build -f build/linux/Dockerfile .
 
-printf "\nInstalling artifacts to artifact repository ...\n\n"
+# According to https://circleci.com/docs/2.0/building-docker-images/
+# > "It is not possible to mount a folder from your job space into a container in Remote Docker (and vice versa). 
+# >  You may use the docker cp command to transfer files between these two environments."
+
+printf "\n>> Creating a dummy Docker container to hold a volume for all project files\n\n"
+DATA_CONTAINER=phoneme-preverifier-data
+ALREADY_EXISTS="$(docker ps --all --no-trunc --quiet --filter name=^$DATA_CONTAINER$)"
+if [ ! -z "$ALREADY_EXISTS" ]; then
+  printf "\n>> Old data container still exists, removing it ...\n\n"
+  $WINPTY docker rm --force --volumes $DATA_CONTAINER
+fi
+$WINPTY docker create -v /project --name $DATA_CONTAINER debian:9.9 bash
+
+printf "\n>> Copying project files into this volume ...\n\n"
+$WINPTY docker cp . $DATA_CONTAINER:"/project"
+
+printf "\n>> Running Docker image to compile binaries for Linux ...\n\n"
+$WINPTY docker run --volumes-from $DATA_CONTAINER -it --rm phoneme-preverifier-build
+
+printf "\n>> Copying build artifacts from data container volume ...\n\n"
+$WINPTY docker cp $DATA_CONTAINER:project/build/linux/preverify ./build/linux/preverify
+
+printf "\n>> Clean up data container and volumes... \n\n"
+$WINPTY docker rm --force --volumes $DATA_CONTAINER
+
+printf "\n>> Installing artifacts to artifact repository ...\n\n"
 ant install
 
-echo "SUCCESS! Binary can be found at: build/linux/preverifier"
+printf "\n>> SUCCESS! Binary can be found at: build/linux/preverifier \n\n"
--- build/linux/build-image.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -e
-
-DOCKER_BUILDKIT=1 docker build --tag=phoneme-preverifier-build -f build/linux/Dockerfile .
